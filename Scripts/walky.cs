27852251d684734695dabd2265c2bba2c8dcc828
fluoride <rain@buggirls.xyz>
2026-01-27 17:06:45 +0000

    h8uibkn

--- Scripts/walky.cs
+++ Scripts/walky.cs
@@ -5,21 +5,46 @@ public partial class walky : CharacterBody2D
 {
 	[Export] 
 	public int Speed { get; set; } = 400;
+	public float AudioMultiplier { get; set; } = 1f;
+	
+	
 	
 	public void GetInput()
 	{
 		LookAt(GetGlobalMousePosition());
 		Vector2 inputDirection = Input.GetVector("left", "right", "forward", "back");
 		//GD.Print(Transform.X.AngleTo(inputDirection));
-		float multi = Math.Clamp((GlobalPosition.DistanceTo(GetGlobalMousePosition()) / 500), 0f, 400f);
+		float SpeedMulti = Math.Clamp((GlobalPosition.DistanceTo(GetGlobalMousePosition()) / 500), 0f, 400f);
 		//GD.Print(Speed);
 		if (Math.Abs(Transform.X.AngleTo(inputDirection)) > Math.PI / 3f)
-			Velocity = inputDirection * Math.Clamp((Speed/3f / multi), 0, 400);
+			Velocity = inputDirection * Math.Clamp((Speed/3f / SpeedMulti), 0, 400);
 		else
 			Velocity = inputDirection * Speed;
 		
-		GD.Print(Math.Clamp((Speed/3f/multi), 0, 400));
+		//GD.Print(Math.Clamp((Speed/3f/SpeedMulti), 0, 400));
+		
+		var thing = new CollisionShape2D();
+		thing.Name = "AUDIOCUE";
+		thing.Shape = new CircleShape2D();
+
+
+		Root.AddChild(thing);
+		var ExpireAudio = GetNode("ExpireAudio");
+		thing.Scale = new Vector2(this.Scale.X*2, this.Scale.Y*2);
+		thing.Position = Vector2.Zero;
+
+
+
 	}
+
+	void _on_timer_timeout()
+	{
+		GD.Print("HIII");
+		this.FindChild("AUDIOCUE").Free();
+		this.FindChild("Timer").Free();
+	}
+
+	
 
 	public override void _PhysicsProcess(double delta)
 	{
